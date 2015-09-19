d54cd121fa19b3e055020f649750eb2bd62a1dc9
chescaumeno <chesca.umeno@hotmail.com>
2015-09-19 14:42:30 -0500

    added code

---
+++ HelloWorld/src/core/HelloWorld.java
@@ -0,0 +1,13 @@
+package core;
+/**
+ * A simple program familiar to all programmers
+ * @author Chesca (Anna F. Umeno)
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
